411ada4c1c445dbb34e2c6dbbe30bcb5f6ac9649
Bryan <artificialknowledge217@gmail.com>
2017-04-04 21:48:30 -0400

    create route.delete

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -31,6 +31,28 @@ router.post('/', jsonParser, (req, res) => {
 router.delete('/:id', (req, res) => {
 	BlogPosts.delete(req.params.id);
 	res.status(204).end();
-})
+});
+
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = [
+  	'id', 'title', 'content', 'author', 'publishDate'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      return res.status(400).send(`${field} is missing`);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    return res.status(400).send(`Ids need to match.`);
+  }
+  const updatedItem = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+  });
+  res.status(204).json(updatedItem);
+});
 
 module.exports = router;
