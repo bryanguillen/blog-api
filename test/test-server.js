927741efe3c621b903d052c76a4b37e0a57348ea
Bryan <artificialknowledge217@gmail.com>
2017-04-05 20:31:14 -0400

    should GET all blog posts (test)

--- test/test-server.js
+++ test/test-server.js
@@ -16,4 +16,22 @@ describe('Blog Post API Endpoints', function() {
 	after(function() {
     	return closeServer();
   	});
+
+  	it('should GET all blog post resources', function() {
+  		return chai.request(app)
+  			.get('/blog-posts')
+  			.then(function(res) {
+  				res.should.have.status(200);
+  				res.should.be.json;
+  				res.body.should.be.a('array');
+  				res.body.length.should.be.at.least(1);
+  				const expectedKeys = ['id', 'title', 'content', 'author', 'publishDate'];
+  				res.body.forEach(function(item) {
+  					item.should.be.a('object');
+  					item.should.include.keys(expectedKeys);
+  				});
+  			});
+  	});
+
+
 });
