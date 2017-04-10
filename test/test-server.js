aed5ee3084561f12bb9c02114a62ad88e1cd456f
Bryan <artificialknowledge217@gmail.com>
2017-04-10 11:57:18 -0400

    create GET ALL RESOURCES TEST

--- test/test-server.js
+++ test/test-server.js
@@ -1,29 +1,34 @@
 //imports and requirements
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const faker = require('faker');
 const {app, runServer, closeServer} = require('../server');
+const {BlogPost} = require('../model')
 const should = chai.should();
+const mongoose = require('mongoose');
+const {TEST_DATABASE_URL} = require('../config');
 
 //allowing for http integration testing
 chai.use(chaiHttp);
 
-function createPost() {
+function randomPost() {
 	let post = {
 		title: "Random Post",
 		content: "Lorem Ipsum",
 		author: {
-			faker.firstName.firstName(),
-			faker.lastName.lastName()
-		},
+      		firstName: faker.name.firstName(),
+      		lastName: faker.name.lastName()
+    	},
 		publishDate: Date.now()
-	}
+	};
+	return post;
 }
 
 function seedDatabase() {
 	let seedData = [];
-	for(let i=0; i<10; ++) {
-		seedData.push(createPost());
-	}
+	for(let i=0; i<10; i++) {
+		seedData.push(randomPost());
+	};
 	return BlogPost.insertMany(seedData);
 }
 
@@ -34,84 +39,39 @@ function tearDownDatabase() {
 describe('Blog Post API Endpoints', function() {
 //before makes sure the server is running and after makes sure to close it to stop any funny behavior
  	before(function() {
-     	return runServer();
+     	return runServer(TEST_DATABASE_URL);
   	});
 
  	beforeEach(function() {
  		return seedDatabase();
  	});
 
- 	after(function() {
-     	return closeServer();
-   	});
-
  	afterEach(function() {
  		return tearDownDatabase();
  	})
 
-//   	it('should GET all blog post resources', function() {
-//   		return chai.request(app)
-//   			.get('/blog-posts')
-//   			.then(function(res) {
-//   				res.should.have.status(200);
-//   				res.should.be.json;
-//   				res.body.should.be.a('array');
-//   				res.body.length.should.be.at.least(1);
-//   				const expectedKeys = ['id', 'title', 'content', 'author', 'publishDate'];
-//   				res.body.forEach(function(item) {
-//   					item.should.be.a('object');
-//   					item.should.include.keys(expectedKeys);
-//   				});
-//   			});
-//   	});
+ 	after(function() {
+     	return closeServer();
+   	});
 
-//   	it('should POST new blog post resource', function() {
-//   		const newPost = {
-//   			title: "Hello Universe", 
-//   			content: "I am the hello world enemy", 
-//   			author: "Bryan G", 
-//   			publishDate: "4/5/2017"
-//   		};
-//   		return chai.request(app)
-//   			.post('/blog-posts')
-//   			.send(newPost)
-//   			.then(function(res) {
-//   				res.should.have.status(201);
-//   				res.should.be.json;
-//   				res.body.should.be.a('object');
-//   				res.body.should.include.keys('title', 'content', 'author', 'publishDate');
-//   				res.body.id.should.not.be.null;
-
-//   			});
-//   	});	
-
-//   	it('should PUT / update existing blog post', function() {
-//   		const updateData = {
-//       		title: 'foo',
-//       		content: 'hello world',
-//       		author: 'Bryan Guillen',
-//       		publishDate: '4/5/2017'
-//     	};
-
-//     	return chai.request(app)
-//       		.get('/blog-posts')
-//       		.then(function(res) {
-//         		updateData.id = res.body[0].id;
-//         		return chai.request(app)
-//           			.put(`/blog-posts/${updateData.id}`)
-//           			.send(updateData);
-//       		});
-//   	});
-
-//   	it('should DELETE existing blog-posts', function() {
-//   		return chai.request(app)
-//       		.get('/blog-posts')
-//       		.then(function(res) {
-//         		return chai.request(app)
-//           			.delete(`/blog-posts/${res.body[0].id}`);
-//       		})
-//       		.then(function(res) {
-// 		        res.should.have.status(204);
-//       		});
-//   	});
-// });
+ 	describe('GET ALL RESOURCES', function() {
+ 		//testing strategy
+ 		//should get all of the resources from endpoint
+ 		let res;
+ 		it('should show all posts', function() {
+ 			return chai.request(app)
+ 				.get('/posts')
+ 				.then(function(_res) {
+ 					res = _res;
+ 					res.should.have.status(200);
+ 					res.should.be.json;
+ 					res.body.should.be.a('object');
+ 					return BlogPost.count();
+ 				})
+ 				then(function(count) {
+ 					//make sure there is consistency between db and the response object length
+ 					res.body.posts.should.have.length.of(count);
+ 				});
+ 		});
+ 	})
+});
